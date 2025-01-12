cb18bd1913cfc6103867c47b45ee5ead8cd49c6b
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2025-01-12 18:36:05 +0100

    5th

---
+++ app.js
@@ -0,0 +1,9 @@
+const carrito = document.getElementById("carrito");
+const template = document.getElementById("template");
+const fragment = document.createDocumentFragment();
+const btnesBotones = document.querySelectorAll(".card .btn");
+
+console.log(carrito);
+console.log(template);
+console.log(fragment);
+console.log(btnesBotones);
