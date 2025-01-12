6c338cc6f42cb2510708e3abdd5c2774093a40aa
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2025-01-12 18:46:34 +0100

    7th

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const btnesBotones = document.querySelectorAll(".card .btn");
 const carritoObjeto = {};
 
 const agregarAlCarrito = (e) => {
-    console.log(e.target);
+    console.log(e.target.dataset.fruta);
 }
 
 btnesBotones.forEach(btn => btn.addEventListener('click', agregarAlCarrito));
