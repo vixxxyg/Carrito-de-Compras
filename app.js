d29a1f626fa93b2ec422a40158f86f24741f36b1
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2025-01-12 19:24:02 +0100

    9th

--- app.js
+++ app.js
@@ -13,14 +13,23 @@ const agregarAlCarrito = (e) => {
     const producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
-        cantidad: 1
+        cantidad: 1,
     }
 
     carritoObjeto[producto.titulo] = producto;
-
-    console.log(carritoObjeto);
+    pintarCarrito(producto);
 };
+
 const pintarCarrito = (producto) => {
-    console.log("pintar carrito", producto);
-}
+    Object.values(carritoObjeto).forEach(
+        (item) => {
+            const clone = template.content.firstElementChild.cloneNode(true);
+            clone.querySelector('.lead').textContent = item.titulo;
+            clone.querySelector('.badge').textContent = item.cantidad;
+        
+            fragment.appendChild(clone);
+        })
+    carrito.appendChild(fragment);
+};
+
 btnesBotones.forEach(btn => btn.addEventListener('click', agregarAlCarrito));
